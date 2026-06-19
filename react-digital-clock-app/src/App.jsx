fbe691c3e1ffa05636c03aa58eedcac47b3b7215
ardhra <meardhra21@gmail.com>
2026-06-19 12:35:08 +0530

    React Digital Clock

---
+++ react-digital-clock-app/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from "react";
+
+function DigitalClock() {
+  const [time, setTime] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(new Date());
+    }, 1000);
+
+    // Cleanup interval when component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const formattedTime = time.toLocaleTimeString("en-US", {
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true,
+  });
+
+  return (
+    <div>
+      <h2>Current Time:</h2>
+      <h1>{formattedTime}</h1>
+    </div>
+  );
+}
+
+export default DigitalClock;
